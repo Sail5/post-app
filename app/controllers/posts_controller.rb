a0c51a313ee60e7628aded7f34315f6b50b97afb
Ryo Hirata <144773550+Sail5@users.noreply.github.com>
2023-11-11 23:31:31 +0900

    Add files via upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.all.order(created_at: :desc)
     end
 
     def new
@@ -21,8 +21,43 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    #def update
+    #    @post = Post.find(params[:id])
+    #   if @post.update(post_params)
+    #      redirect_to posts_path
+    #    else
+    #      render :edit
+    #    end
+    #end
+
+    #def destroy
+    #    @post = Post.find(params[:id])
+     #   @post.destroy
+    #    redirect_to posts_path
+        
+    #end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        respond_to do |format|
+          format.html { redirect_to posts_path }
+          format.js   # 追加
+        end
+    end
+    def image
+        @post = Post.find(params[:id])
+        send_data @post.image.download, type: @post.image.content_type, disposition: 'inline'
+    end
     def update
         @post = Post.find(params[:id])
+    
+        # Check if a new image is being attached
+        if params[:post][:new_image].present?
+          @post.image.purge if @post.image.attached?  # Remove the old image
+          @post.image.attach(params[:post][:new_image])  # Attach the new image
+        end
+    
         if @post.update(post_params)
           redirect_to posts_path
         else
@@ -30,17 +65,14 @@ class PostsController < ApplicationController
         end
     end
 
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path
-    end
-
-
 
     private
 
     def post_params
-        params.require(:post).permit(:title, :content)
+        params.require(:post).permit(:title, :content, :image)
     end
 end
+
+class Post < ApplicationRecord
+    has_one_attached :image
+  end
