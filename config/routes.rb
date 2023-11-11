87c4857b6791d7a7890970e4555b988eea7c995b
Ryo Hirata <144773550+Sail5@users.noreply.github.com>
2023-11-11 23:34:25 +0900

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :posts do
+    member do
+      get 'image'
+    end
+  end
 end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
